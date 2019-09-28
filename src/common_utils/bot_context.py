9dfe75170da522dd0076602978917c5e6582aff4
rpych <dragon0710@wp.pl>
2019-09-28 13:28:50 +0200

    add method context_update to BotContext class

--- src/common_utils/bot_context.py
+++ src/common_utils/bot_context.py
@@ -1,6 +1,20 @@
+from src.common_utils.types_of_conversation import TypeOfOperation
+
+
 class BotContext:
-    speaker_name = None
-    is_after_greeting = False
-    is_after_introduction = False
-    is_name_request_processed = False
-    is_after_name_response_reaction = False
+    def __init__(self):
+        self.speaker_name = None
+        self.is_after_greeting = False
+        self.is_after_introduction = False
+        self.is_name_request_processed = False
+        self.is_after_name_response_reaction = False
+
+    def context_update(self, type_of_conversation):
+        if type_of_conversation == TypeOfOperation.NAME.value:
+            self.is_name_request_processed = True
+        elif type_of_conversation == TypeOfOperation.GREETING.value:
+            self.is_after_greeting = True
+        elif type_of_conversation == TypeOfOperation.NAME.value:
+            self.is_after_introduction = True
+        elif type_of_conversation == TypeOfOperation.CONTEXT_NAME.value:
+            self.is_after_name_response_reaction = True
