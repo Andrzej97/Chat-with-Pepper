54ae452216d9ad9bc6e5d7e4103b53967bc004b5
rpych <dragon0710@wp.pl>
2019-10-05 12:49:43 +0200

    minor fixes in ChatbotManager and speech recognition demo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from src.main_chat.chatbot_manager import ChatbotManager
 
 def main():
 
-    chatbot_manager = ChatbotManager(general_chatbot='Bolek', university_chatbot='Lolek',
+    chatbot_manager = ChatbotManager(intro_chatbot='Bolek', university_chatbot='Lolek',
                                      connection_uri='mongodb://localhost:27017/', database_name='PepperChatDB')
     while True:
         user_input = input('>>>')
--- src/main_chat/chatbot_manager.py
+++ src/main_chat/chatbot_manager.py
@@ -1,5 +1,4 @@
 from src.common_utils.bot_context import BotContext
-from src.common_utils.language_utils.sentence_filter_utils import SentenceFilter
 from src.general_chatbot.intro_conversation_bot import IntroBot
 from src.university_chatbot.university_conversation_bot import UniversityBot
 from src.common_utils.database_service import DatabaseProxy
@@ -7,13 +6,12 @@ from src.common_utils.database_service import DatabaseProxy
 
 class ChatbotManager:
     def __init__(self, **kwargs):
-        self._intro_chatbot_name = kwargs.get('general_chatbot', 'Żwirek')  # our chatbots code names
+        self._intro_chatbot_name = kwargs.get('intro_chatbot', 'Żwirek')  # our chatbots code names
         self._university_chatbot_name = kwargs.get('university_chatbot', 'Muchomorek')
         db = DatabaseProxy(kwargs.get('connection_uri'), kwargs.get('database_name'))
         bot_context = BotContext()
         self._intro_chatbot = IntroBot(self._intro_chatbot_name, bot_context, db)
         self._university_chatbot = UniversityBot(self._university_chatbot_name, db)
-        self._sentence_filter = SentenceFilter()
         self._is_intro_bot_unemployed = False
 
     def _ask_intro_chatbot(self, processed_sentence):
