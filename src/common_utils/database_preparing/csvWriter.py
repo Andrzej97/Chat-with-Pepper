d3bf93de41b92297b3b6e6e54a0a72af8b26a5b0
Andrzej97 <szafand@gmail.com>
2019-10-26 23:21:54 +0200

    Scrapper refactoring

--- src/common_utils/database_preparing/csvWriter.py
+++ src/common_utils/database_preparing/csvWriter.py
@@ -14,7 +14,3 @@ class CsvWriter:
         with open(self.filename, encoding="utf-8", mode="a", newline='') as file:
             csv_writer = csv.writer(file, delimiter=DELIMITER)
             csv_writer.writerow(row)
-
-csvWriter = CsvWriter('testFile.csv')
-csvWriter.write_tags_and_text(['uczelnia', 'agh'], 'AGH to super uczelnia')
-csvWriter.write_tags_and_text(['wydziały', 'agh'], 'AGH ma kilka wydziałów')
