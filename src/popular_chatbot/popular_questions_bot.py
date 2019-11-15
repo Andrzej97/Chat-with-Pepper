11432359e5c7a232fa7f25930e11f3f687dadefe
rpych <dragon0710@wp.pl>
2019-11-15 11:22:22 +0100

    add popular_chatbot package with classes to serve the most popular questions which may appear and questions like "How many..."

---
+++ src/popular_chatbot/popular_questions_bot.py
@@ -0,0 +1,26 @@
+from chatterbot import ChatBot
+
+
+class PopularQuestionsBot:
+    def __init__(self, name, db_proxy):
+        self.name = name
+        self._bot = self.initialize_chatbot(db_proxy)
+
+    def get_bot(self):
+        return self._bot
+
+    def initialize_chatbot(self, db_proxy):
+        return ChatBot(
+            self.name,
+            logic_adapters=[
+                {
+                    'import_path': 'src.popular_chatbot.numbers_questions_logic_adapter'
+                                   '.UniversityAdapter',
+                    'database_proxy': db_proxy
+                },
+            ],
+        )
+
+    def get_bot_response(self, input):
+        response = self._bot.get_response(input)
+        return response
---
+++ src/popular_chatbot/popular_questions_logic_adapter.py
@@ -0,0 +1,19 @@
+from chatterbot.conversation import Statement
+from chatterbot.logic import LogicAdapter
+
+import src.common_utils.language_utils.statement_utils as statement_utils
+from src.common_utils.language_utils.sentence_filter_utils import SentenceFilter
+
+class PopularQuestionsAdapter(LogicAdapter):
+    def __init__(self, chatbot, **kwargs):
+        super().__init__(chatbot, **kwargs)
+        self.db = kwargs.get('database_proxy')
+        self.sentence_filter = SentenceFilter()
+
+
+    def can_process(self, statement):
+        return True
+
+
+    def process(self, statement, additional_response_selection_parameters=None):
+        pass
