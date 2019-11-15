0fa451d59e52e79403b84235ced067e1e2ed4b45
rpych <dragon0710@wp.pl>
2019-11-15 21:05:27 +0100

    add database initialization script

--- init_project.sh
+++ init_project.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 ROOT_DIR=`pwd`
-echo -e "You are in $ROOT_DIR directory"
 export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
-echo -e "PYTHONPATH is = $PYTHONPATH"
 cd src/common_utils/database_preparing
 CURR_DIR=`pwd`
-echo -e "You are now in $CURR_DIR directory"
 export PYTHONPATH=$PYTHONPATH:$CURR_DIR
-echo -e "PYTHONPATH now is = $PYTHONPATH"
 python3 set_init_configure.py
 cd ../../..
-echo -e "You are back in project root folder"
 
--- src/common_utils/database_preparing/set_init_configure.py
+++ src/common_utils/database_preparing/set_init_configure.py
@@ -1,6 +1,7 @@
 import src.common_utils.database_preparing.initialize_database as general_data
 from src.common_utils.database_service import DatabaseProxy
 import src.common_utils.database_preparing.csv_to_database_filler as scrapper_data
+import src.common_utils.custom_exceptions as Exceptions
 
 
 def initialize_language_utils_database(db):
@@ -8,12 +9,15 @@ def initialize_language_utils_database(db):
         run this method just when you use this code first time to initialize database with words from file
     """
     path = "./polish_stopwords.txt"  # in case of errors make sure that path is ok, `os.getcwd()` command is useful
-    #db.create_new_collection('polish_stop_words')
     result = from_txt_file_to_list(path)
     list = []
     for r in result:
         list.append({'text': r})
-    db.add_many_new_docs_to_collection('polish_stop_words', list)
+    try:
+        db.create_new_collection('polish_stop_words')
+        db.add_many_new_docs_to_collection('polish_stop_words', list)
+    except Exceptions.CollectionAlreadyExistsInDatabaseError:
+        db.add_many_new_docs_to_collection('polish_stop_words', list)
 
 
 def from_txt_file_to_list(path):
