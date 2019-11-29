05be876ba8fe302b9188b649d566304656d53875
rpych <dragon0710@wp.pl>
2019-11-30 00:03:38 +0100

    add functions/methods for handling inserting popular_questions_chatbot collections to database

--- src/common_utils/database_preparing/set_init_configure.py
+++ src/common_utils/database_preparing/set_init_configure.py
@@ -1,41 +1,11 @@
 import src.common_utils.custom_exceptions as Exceptions
-import src.common_utils.database_preparing.csv_to_database_filler as scrapper_data
-import src.common_utils.database_preparing.init_db_with_general_data as general_data
-import src.common_utils.database_preparing.init_db_with_popular_data as popular_data
+import src.common_utils.database_preparing.csv_to_database_filler as csv_data
 from configuration import Configuration as conf
 from src.common_utils.database.database_service import DatabaseProxy
 import src.common_utils.custom_exceptions as Exceptions
 
 
-def initialize_db_with_continue_statements(db):
-    # fixme: to be removed after adding to database initialization file
-
-    db.add_conversation(text="powiedzieć", tag='continue')
-    db.add_conversation(text="więcej", tag='continue')
-    # ------------------------------------------------------
-    db.add_conversation(text="przykro mi, to wszystko co wiem", tag='cannot_say_more')
-    db.add_conversation(text="to wszystko co wiem na ten temat", tag='cannot_say_more')
-    db.add_conversation(text="nie wiem więcej", tag='cannot_say_more')
-    db.add_conversation(text="na tę chwilę to musi wystarczyć", tag='cannot_say_more')
-
-
-def initialize_db_with_popular_data(db):
-    COLLECTION_NAME = conf.NUMBERS_QUEST_COLLECTION.value
-    try:
-        popular_data.initialize_db_with_numbers_questions(db, COLLECTION_NAME)
-    except Exceptions.CollectionAlreadyExistsInDatabaseError:
-        db.remove_collection(COLLECTION_NAME)
-        popular_data.initialize_db_with_numbers_questions(db, COLLECTION_NAME)
-
-    COLLECTION_NAME = conf.POPULAR_QUEST_COLLECTION.value
-    try:
-        popular_data.initialize_db_with_popular_questions(db, COLLECTION_NAME)
-    except Exceptions.CollectionAlreadyExistsInDatabaseError:
-        db.remove_collection(COLLECTION_NAME)
-        popular_data.initialize_db_with_popular_questions(db, COLLECTION_NAME)
-
-
-def initialize_language_utils_database(db):
+def initialize_polish_stopwords_collection(db):
     """
         run this method just when you use this code first time to initialize database with words from file
     """
@@ -58,7 +28,7 @@ def from_txt_file_to_list(path):
 
 
 def create_collections(database):
-    collections = [collection for collection in list(Configuration) if contains('collection', collection.name)]
+    collections = [collection for collection in list(conf) if contains('collection', collection.name)]
     capped_collections = [capped_collection for capped_collection in collections if
                           contains('capped', capped_collection.name)]
     for collection in collections:
@@ -83,14 +53,11 @@ def contains(key, parameter):
 def main():
     db = DatabaseProxy('mongodb://localhost:27017/', 'PepperChatDB')
     create_collections(db)
-    initialize_language_utils_database(db)
-    general_data.init_database(db)
-    #scrapper_data.initialize_main_collection_from_scrapper(db)
-    initialize_db_with_continue_statements(db)
-    initialize_db_with_popular_data(db)
-    initialize_language_utils_database(db)
-    scrapper_data.initialize_main_collection_from_scrapper(db)
-    scrapper_data.inert_into_database('./csv_files/main_statements.csv', db)
+    initialize_polish_stopwords_collection(db)
+    csv_data.initialize_main_collection_from_scrapper(db)
+    csv_data.insert_into_database('./csv_files/main_statements.csv', db)
+    csv_data.insert_into_database('./csv_files/numbers_log_adapter_init_data.csv', db, conf.NUMBERS_QUEST_COLLECTION.value)
+    csv_data.insert_into_database('./csv_files/popular_log_adapter_init_data.csv', db, conf.POPULAR_QUEST_COLLECTION.value)
 
 
 
